b0cab83bb2e87927da4851de01825f6c7611d424
Shiva82804 <shivaaithagoni111@gmail.com>
2025-07-10 11:40:16 +0530

    10-07-2025 Info

--- 3440_Solution.java
+++ 3440_Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxFreeTime(int eventTime, int[] startTime, int[] endTime) {
         int n = startTime.length;
         boolean[] q = new boolean[n];
-        for(int i = 0,t1 = 0,t2 = 0;i<n;i++){
+        for(int i = 0,t1 = 0,t2 = 0;i<n;i++){     //Checking the avaiability of each event to reschedule
             if(endTime[i]-startTime[i]<=t1){
                 q[i] = true;
             }
@@ -14,8 +14,8 @@ class Solution {
         }
         int ans = 0;
         for(int i = 0;i<n;i++){
-            int left = i==0 ? 0 : endTime[i-1];
-            int right = i==n-1 ? eventTime : startTime[i+1];
+            int left = i==0 ? 0 : endTime[i-1];               //edge case for left     
+            int right = i==n-1 ? eventTime : startTime[i+1];  //edge case for right
             if(q[i]){
                 ans = Math.max(ans,right-left);
             }else{
